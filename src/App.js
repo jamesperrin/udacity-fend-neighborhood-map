f7f27cf3b13ce62ef417f7c9c2cd78ef7e24a2e1
James Perrin <countrydawgg@gmail.com>
2018-10-23 13:38:12 -0700

    (1) Add Feature Map Marker filtering. Filter map markers based on venues list. (2) Update styling namings.

--- src/App.js
+++ src/App.js
@@ -1,80 +1,105 @@
 import React, { Component } from 'react';
 import './App.css';
-import FoursquareAPI from './FoursquareAPI'
-import ListLocations from './ListLocations'
-import config from './config/config'
+import config from './config/config';
+import FoursquareAPI from './FoursquareAPI';
+import ListVenues from './ListVenues';
 
 class App extends Component {
 
     state = {
-        venues_test : [
-            {name: 'Café du Monde', location: {lat: 29.957451318025385, lng: -90.06190001964569}},
-            {name: 'Cafe Beignet', location: {lat: 29.955215743004878, lng: -90.06716251373291}},
-            {name: 'Palace Café', location: {lat: 29.952902, lng: -90.068193}},
-            {name: 'Cafe Pontalba', location: {lat: 29.957258, lng: -90.063818}},
-            {name: 'The Market Cafe', location: {lat: 29.95910391865961, lng: -90.06057170840525}},
-            { name: 'Cafe Maspero', location: { lat: 29.956004680661643, lng: -90.0634610652136 } }
-        ],
-        venues: [],
-        markers: [],
-        mapMarkers: []
+        venues_fakes : [
+            { name: 'Café du Monde', id: "4aa59477f964a520dd4820e3", location: {lat: 29.957451318025385, lng: -90.06190001964569} },
+            { name: 'Cafe Beignet', id: "4ad4c04df964a52072f320e3", location: {lat: 29.955215743004878, lng: -90.06716251373291} },
+            { name: 'Palace Café', id: "4ad4c050f964a520a0f420e3", location: {lat: 29.952902, lng: -90.068193} },
+            { name: 'Cafe Pontalba', id: "4bf58dd8d48988d17a941735", location: {lat: 29.957258, lng: -90.063818} },
+            { name: 'The Market Cafe', id: "4ae25ec4f964a520be8d21e3", location: {lat: 29.95910391865961, lng: -90.06057170840525} },
+            { name: 'Cafe Maspero', id: "4aecf62ef964a52076cc21e3", location: { lat: 29.956004680661643, lng: -90.0634610652136 } }
+        ], // Used for testing
+        venues: [], // List of Venues
+        mapMarkers: [] // Used for accesssing map Markers
     };
 
     /**
      * @description Retrieves list of Venues from Foursquare API
      */
     getVenues() {
-        let params =             {
+        let searchParams = {
             'near': '29.957431,-90.0629443',
             'query': 'cafe',
             'limit': 10
-        }
+        };
 
-        FoursquareAPI.search(params).then(data => {
-            const venues = data.response.venues;
-            const markers = venues.map(venue => {
-                return {
-                    title: venue.name,
-                    id: venue.id,
-                    position: {
-                        lat: venue.location.lat,
-                        lng: venue.location.lng
-                    },
-                    isVisiable: true
-                };
+        // Using Foursquare API to retrieve list of Venues
+        FoursquareAPI
+            .search(searchParams)
+            .then(results => {
+                const venues = results.response.venues;
+
+                // Setting state
+                this.setState({ venues }, this.renderMap());
+            }).catch(err => {
+                console.error(err);
             });
+    }
 
-            //DEBUG CODE
-            // console.log(venues);
-            // console.log(markers);
-
-            // Setting state
-            this.setState({ venues, markers}, this.renderMap());
-        }).catch(err => {
-            console.error(err);
+    /**
+     * @description Renders Google Map
+     */
+    renderMap() {
+        const { apiKey } = config.GoogleMap;
+        const script = document.createElement('script');
+        script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}`;
+        script.async = true;
+        script.defer = true;
+        script.addEventListener('load', () => {
+            this.initMap();
         });
+
+        // Addin script to web page
+        document.body.appendChild(script);
     }
 
     /**
      * @description Google Map initialization
      */
     initMap() {
-        const { venues } = this.state;
-        let mapMarkers = [];
-        const infoWindow = new window.google.maps.InfoWindow();
-
         // Create google map to display
         const map = new window.google.maps.Map(document.getElementById('map'), {
             center: { lat: 29.957431, lng: -90.062944 },
             title: 'Udacity FEND Neighborhood Map',
             zoom: 15,
-            mapTypeId: 'roadmap',
+            mapTypeId: 'roadmap'
         });
 
-        // Filter map markers
-        this.state.markers && this.state.markers
-            .filter(marker => marker.isVisiable)
-            .forEach(marker => { 
+        // Creates Map Markers
+        this.createMapMarkers(map);
+    }
+
+    /**
+     * @description Create Google Map Markers
+     * @param {Object} map Reference to Google Map
+     */
+    createMapMarkers(map) {
+        const { venues } = this.state;
+        const infoWindow = new window.google.maps.InfoWindow();
+        const bounds = new window.google.maps.LatLngBounds();
+        let mapMarkers = [];
+
+        // Shaping Venues' data
+        const venuesData = venues.map(venue => {
+            return {
+                title: venue.name,
+                id: venue.id,
+                position: {
+                    lat: venue.location.lat,
+                    lng: venue.location.lng
+                }
+            };
+        });
+
+        // Creating map markers
+        venuesData
+            .forEach(marker => {
                 let mapMarker = new window.google.maps.Marker({
                     map: map,
                     animation: window.google.maps.Animation.DROP,
@@ -88,79 +113,108 @@ class App extends Component {
                     // Retrieve venue details
                     FoursquareAPI.getVenueDetails(venue.id)
                         .then(res => res)
-                        .then(data => {
-                            const result = data.response.venue;
-                            const name = result.name;
-                            const photo = `${result.bestPhoto.prefix}380x280${result.bestPhoto.suffix}`;
-                            const adress = result.location.address;
-                            const cityState = `${result.location.city}, ${result.location.state} ${result.location.postalCode}`;
-                            const formattedPhone = result.contact.formattedPhone;
+                        .then(results => {
+                            const result = results.response.venue;
 
-                            const contentString = `<div class="infoWindow"><strong>${name}</strong><p>${adress}</p><p>${cityState}</p><p>${formattedPhone}</p><hr /><img src="${photo}" alt="Venue Image" /><div/>`;
+                            // Shaping Venue details
+                            const venueDetails = {
+                                name: result.name,
+                                adress: result.location.address,
+                                cityStateZip: `${result.location.city}, ${result.location.state} ${result.location.postalCode}`,
+                                phone: result.contact.formattedPhone,
+                                photo: `${result.bestPhoto.prefix}285x210${result.bestPhoto.suffix}`
+                            };
+
+                            // Creating InfoWindow content
+                            const contentString = `<div class="infoWindow"><strong>${venueDetails.name}</strong><p>${venueDetails.adress}</p><p>${venueDetails.cityStateZip}</p><p>${venueDetails.phone}</p><hr /><img src="${venueDetails.photo}" alt="Venue Image" /><div/>`;
 
                             infoWindow.setContent(contentString);
                         }).catch(err => { 
                             console.error(err);
-                            const contentString = `<div class="infoWindow"><strong>${venue.name}</strong><div/>`;
+
+                            // Using catch block in case of hitting Foursquare API premium rate limit.
+                            // Allows testing of app
+
+                            // Shaping Venue details
+                            const venueDetails = {
+                                name: venue.name,
+                                adress: venue.location.address,
+                                cityStateZip: `${venue.location.city}, ${venue.location.state} ${venue.location.postalCode}`,
+                                phone: venue.contact.formattedPhone
+                            };
+
+                            // Creating InfoWindow content
+                            const contentString = `<div class="infoWindow"><strong>${venueDetails.name}</strong><p>${venueDetails.adress}</p><p>${venueDetails.cityStateZip}</p><p>${venueDetails.phone}</p><div/>`;
 
                             infoWindow.setContent(contentString);
-
                         });// END FoursquareAPI.getVenueDetails()
 
-                    // Open InfoWindow
+                    // Open InfoWindow event
                     infoWindow.open(map, mapMarker);
-                });
+                }); // END mapMarker.addListener('click')
 
                 // Add marker to array of map markers
                 mapMarkers.push(mapMarker);
-        });// END markers filter
+
+                // Setting map boundaries
+                bounds.extend(mapMarker.position);
+        });// END venuesMarkers.forEach()
+
+        // Sizing map based on set boundaries
+        map.fitBounds(bounds);
 
         // Setting state
         this.setState({mapMarkers});
     }
 
     /**
-     * @description Handles Venue list click event.
-     * @param venue_id Venue ID
+     * @description Handles Venue list item click event.
+     * @param {string} venueId Venue's ID
      */
-    handleListItemClick = (venue_id) => {
+    handleListItemClick = (venueId) => {
         const { mapMarkers } = this.state;
-        const mapMarkerIndex = mapMarkers.findIndex(marker => marker.id === venue_id);
+        const mapMarkerIndex = mapMarkers.findIndex(marker => marker.id === venueId);
         window.google.maps.event.trigger(mapMarkers[mapMarkerIndex], 'click');
-
-        //DEBUG CODE
-        // const marker = mapMarkers.find(marker => marker.id === venue_id);
-        // console.log(marker);
     }
 
     /**
-     * @description Renders Google Map
+     * @description Updates Google Map Markers' visibility
+     * @param {string} venueName - Venue's name
      */
-    renderMap() {
-        const ApiKey = config.GoogleMap.apiKey;
-        const script = document.createElement('script');
-        script.src = `https://maps.googleapis.com/maps/api/js?key=${ApiKey}`;
-        script.async = true;
-        script.defer = true;
-        script.addEventListener('load', () => {
-            this.initMap();
+    updateMarkersVisibility = (venueName) => {
+        const { venues } = this.state;
+        let { mapMarkers } = this.state;
+
+        // Chaning map markers' visible
+        mapMarkers = venues.map(venue => {
+            const mapMarker = mapMarkers.find(marker => marker.id === venue.id);
+            const isMatch = venue.name.toLowerCase().includes(venueName.trim());
+            mapMarker.setVisible(isMatch);
+
+            return mapMarker;
         });
 
-        document.body.appendChild(script);
+        // Setting state
+        this.setState({ mapMarkers });
     }
 
+    // Component Did mount event
     componentDidMount() {
         this.getVenues();
     }
 
+    // Component Render() event
     render() {
         return (
             <div className="container">
                 <input type='checkbox' id='menu-trigger' className='menu-trigger' />
                 <label htmlFor="menu-trigger"></label>
-                <div className='locations-wrapper'>
+                <div className='venues-wrapper'>
                     <h1>Search for Venues</h1>
-                    <ListLocations handleListItemClick={this.handleListItemClick} venues={this.state.venues} showMenu={this.state.showMenu} />
+                    <ListVenues
+                        handleListItemClick={this.handleListItemClick}
+                        venues={this.state.venues}
+                        updateMarkersVisibility={this.updateMarkersVisibility} />
                 </div>
                 <div id="map"></div>
             </div>
---
+++ src/ListVenues.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
+
+class ListVenues extends Component {
+
+    state = {
+        query: ''
+    }
+
+    /**
+     * @description Updates Venues' name query
+     * @param {string} query - Query for Venues' name
+     */
+    updateQuery = (query) => {
+        // Setting state
+        this.setState({ query: query.trim() });
+
+        // Updating Map Markers' Visibility
+        this.props.updateMarkersVisibility(query.trim());
+    }
+
+    // Component Render() event
+    render() {
+        const { venues, handleListItemClick } = this.props;
+        const { query } = this.state;
+
+        let displayVenues;
+
+        if (query) {
+            const hasMatch = new RegExp(escapeRegExp(query), 'i');
+            displayVenues = venues.filter(venue =>  hasMatch.test(venue.name));
+        } else {
+            displayVenues = venues;
+        }
+
+        displayVenues.sort(sortBy('name'));
+
+        return (
+            <div className='venues-list'>
+                <input type="text"
+                    value={query}
+                    placeholder='Filter venues'
+                    onChange={(e) => this.updateQuery(e.target.value)} />
+                <ul>
+                    {displayVenues.map(venue => (
+                        <li key={venue.id} onClick={() => handleListItemClick(venue.id) }>{venue.name}</li>
+                    ))}
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default ListVenues;
