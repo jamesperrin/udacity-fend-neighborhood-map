0342256215b903d65fc86f3be790213ebf463644
James Perrin <countrydawgg@gmail.com>
2018-10-24 14:10:21 -0700

    (1) Remove unnecessary state items. (2) Add feature indicating which icon has been clicked/selected. (3) Cleaned up code.

--- src/App.js
+++ src/App.js
@@ -7,14 +7,6 @@ import ListVenues from './ListVenues';
 class App extends Component {
 
     state = {
-        venues_fakes : [
-            { name: 'Café du Monde', id: "4aa59477f964a520dd4820e3", location: {lat: 29.957451318025385, lng: -90.06190001964569} },
-            { name: 'Cafe Beignet', id: "4ad4c04df964a52072f320e3", location: {lat: 29.955215743004878, lng: -90.06716251373291} },
-            { name: 'Palace Café', id: "4ad4c050f964a520a0f420e3", location: {lat: 29.952902, lng: -90.068193} },
-            { name: 'Cafe Pontalba', id: "4bf58dd8d48988d17a941735", location: {lat: 29.957258, lng: -90.063818} },
-            { name: 'The Market Cafe', id: "4ae25ec4f964a520be8d21e3", location: {lat: 29.95910391865961, lng: -90.06057170840525} },
-            { name: 'Cafe Maspero', id: "4aecf62ef964a52076cc21e3", location: { lat: 29.956004680661643, lng: -90.0634610652136 } }
-        ], // Used for testing
         venues: [], // List of Venues
         mapMarkers: [] // Used for accesssing map Markers
     };
@@ -103,61 +95,36 @@ class App extends Component {
                 let mapMarker = new window.google.maps.Marker({
                     map: map,
                     animation: window.google.maps.Animation.DROP,
+                    icon: config.GoogleMap.icon.default,
                     ...marker
                 });
-
-                // Add Click event listner to each marker
-                mapMarker.addListener('click', function () { 
-                    const venue = venues.find(venue => venue.id === marker.id);
-
-                    // Retrieve venue details
-                    FoursquareAPI.getVenueDetails(venue.id)
-                        .then(res => res)
-                        .then(results => {
-                            const result = results.response.venue;
-
-                            // Shaping Venue details
-                            const venueDetails = {
-                                name: result.name,
-                                adress: result.location.address,
-                                cityStateZip: `${result.location.city}, ${result.location.state} ${result.location.postalCode}`,
-                                phone: result.contact.formattedPhone,
-                                photo: `${result.bestPhoto.prefix}285x210${result.bestPhoto.suffix}`
-                            };
-
-                            // Creating InfoWindow content
-                            const contentString = `<div class="infoWindow"><strong>${venueDetails.name}</strong><p>${venueDetails.adress}</p><p>${venueDetails.cityStateZip}</p><p>${venueDetails.phone}</p><hr /><img src="${venueDetails.photo}" alt="Venue Image" /><div/>`;
-
-                            infoWindow.setContent(contentString);
-                        }).catch(err => { 
-                            console.error(err);
-
-                            // Using catch block in case of hitting Foursquare API premium rate limit.
-                            // Allows testing of app
-
-                            // Shaping Venue details
-                            const venueDetails = {
-                                name: venue.name,
-                                adress: venue.location.address,
-                                cityStateZip: `${venue.location.city}, ${venue.location.state} ${venue.location.postalCode}`,
-                                phone: venue.contact.formattedPhone
-                            };
-
-                            // Creating InfoWindow content
-                            const contentString = `<div class="infoWindow"><strong>${venueDetails.name}</strong><p>${venueDetails.adress}</p><p>${venueDetails.cityStateZip}</p><p>${venueDetails.phone}</p><div/>`;
-
-                            infoWindow.setContent(contentString);
-                        });// END FoursquareAPI.getVenueDetails()
-
-                    // Open InfoWindow event
-                    infoWindow.open(map, mapMarker);
-                }); // END mapMarker.addListener('click')
 
                 // Add marker to array of map markers
                 mapMarkers.push(mapMarker);
 
                 // Setting map boundaries
                 bounds.extend(mapMarker.position);
+
+                // Add Click event listner to each marker
+                mapMarker.addListener('click', () => {
+                    // Clearing animations
+                    this.clearMarkersAnimation();
+
+                    // Creating InfoWindow
+                    this.createInfoWindow(map, mapMarker, infoWindow);
+                }); // END mapMarker.addListener('click')
+
+                // Mouseover event
+                mapMarker.addListener('mouseover',() => {
+                    mapMarker.setIcon(config.GoogleMap.icon.selected);
+                });
+
+                // Mouseout event
+                mapMarker.addListener('mouseout', () => {
+                    if (infoWindow.marker !== mapMarker) {
+                        mapMarker.setIcon(config.GoogleMap.icon.default);
+                    }
+                });
         });// END venuesMarkers.forEach()
 
         // Sizing map based on set boundaries
@@ -165,6 +132,89 @@ class App extends Component {
 
         // Setting state
         this.setState({mapMarkers});
+    }
+
+    /**
+     * @description Clears Animations for all Map Markers
+     */
+    clearMarkersAnimation = () => {
+        let { mapMarkers } = this.state;
+
+        mapMarkers.forEach(marker => {
+            marker.setIcon(config.GoogleMap.icon.default);
+            marker.setAnimation(null);
+        });
+
+        // Setting state
+        this.setState({mapMarkers});
+    }
+
+    /**
+     * @description Creates and Populates Map Marker InfoWindow
+     * @param {Object} map Reference to Google Map
+     * @param {Object} marker Map Marker
+     * @param {Object} infoWindow Reference to Google Maps InfoWindow
+     */
+    createInfoWindow = (map, marker, infoWindow) => {
+        const { venues } = this.state;
+
+        marker.setIcon(config.GoogleMap.icon.selected);
+        marker.setAnimation(window.google.maps.Animation.BOUNCE);
+
+        if (infoWindow.marker !== marker) {
+            const venue = venues.find(venue => venue.id === marker.id);
+
+            infoWindow.setContent('');
+            infoWindow.marker = marker;
+
+            // Retrieve venue details
+            FoursquareAPI.getVenueDetails(venue.id)
+                .then(res => res)
+                .then(results => {
+                    const result = results.response.venue;
+
+                    // Shaping Venue details
+                    const venueDetails = {
+                        name: result.name,
+                        adress: result.location.address,
+                        cityStateZip: `${result.location.city}, ${result.location.state} ${result.location.postalCode}`,
+                        phone: result.contact.formattedPhone,
+                        photo: `${result.bestPhoto.prefix}285x210${result.bestPhoto.suffix}`
+                    };
+
+                    // Creating InfoWindow content
+                    const contentString = `<div class="infoWindow"><strong>${venueDetails.name}</strong><p>${venueDetails.adress}</p><p>${venueDetails.cityStateZip}</p><p>${venueDetails.phone}</p><hr /><img src="${venueDetails.photo}" alt="${venueDetails.name} Venue Image" /><div/>`;
+
+                    infoWindow.setContent(contentString);
+                }).catch(err => {
+                    console.error(err);
+
+                    // Using catch block in case of hitting Foursquare API premium rate limit.
+                    // Allows testing of app
+
+                    // Shaping Venue details
+                    const venueDetails = {
+                        name: venue.name,
+                        adress: venue.location.address,
+                        cityStateZip: `${venue.location.city}, ${venue.location.state} ${venue.location.postalCode}`
+                    };
+
+                    // Creating InfoWindow content
+                    const contentString = `<div class="infoWindow"><strong>${venueDetails.name}</strong><p>${venueDetails.adress}</p><p>${venueDetails.cityStateZip}</p><div/>`;
+
+                    infoWindow.setContent(contentString);
+                });// END FoursquareAPI.getVenueDetails()
+
+            // Make sure the marker property is cleared if the infowindow is closed.
+            infoWindow.addListener('closeclick', function () {
+                infoWindow.marker = null;
+                marker.setAnimation(null);
+                marker.setIcon(config.GoogleMap.icon.default);
+            });
+
+            // Open InfoWindow event
+            infoWindow.open(map, marker);
+        }
     }
 
     /**
@@ -211,7 +261,7 @@ class App extends Component {
                     <input type="checkbox" id="menu-trigger" className="menu-trigger" />
                     <label htmlFor="menu-trigger"></label>
                     <div className="venues-wrapper">
-                        <h1 role="heading">Search for Venues</h1>
+                        <h1>Search for Venues</h1>
                         <ListVenues
                             handleListItemClick={this.handleListItemClick}
                             venues={this.state.venues}
@@ -219,10 +269,10 @@ class App extends Component {
                         <div id="venue-container-footer">
                             <ul>
                                 <li>
-                                    Data source: <a href="https://developer.foursquare.com/docs/api/endpoints" target="_blank" title="Foursquare Places API">Foursquare Places API</a>
+                                    Data source: <a href="https://developer.foursquare.com/docs/api/endpoints" target="_blank" title="Foursquare Places API" rel="noopener noreferrer">Foursquare Places API</a>
                                 </li>
                                 <li>
-                                    Developer: <a href="https://github.com/jamesperrin" title="James Perrin's GitHub Profile" target="_blank">James Perrin</a>
+                                    Developer: <a href="https://github.com/jamesperrin" title="James Perrin's GitHub Profile" target="_blank" rel="noopener noreferrer">James Perrin</a>
                                 </li>
                             </ul>
                         </div>
